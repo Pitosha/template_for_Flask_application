9b6adfccd7d089011a41a23c575b547c5206b71b
Pitosha <piton20091986@gmail.com>
2020-08-28 04:28:10 +0300

    new commit

--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
 from app import db
 
 class User(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(40))
     surname = db.Column(db.String(40))
-    age = db.Column(db.Integer)
+    user_mail = db.Column(db.String(40))
+    user_role = db.Column(db.String(10))
 
     def __repr__(self):
         return "<User id: {}, User_name: {} User_surname {}>".format(self.id, self.first_name, self.surname)
